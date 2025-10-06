eb41acc38434a0c82672d8249001c69c49c2c20d
JerushaThomas <Jerusha.Thomas@ust.com>
2025-10-06 09:07:05 +0000

    Add HelloWorld project with C and Python implementations
    - Created a new Visual Studio solution file for the HelloWorld project.
    - Added C implementation of Hello World in src/Hello_World.c.
    - Added Python implementation of Hello World in src/Hello_World.py.
    - Generated necessary NuGet configuration files for project dependencies.
    - Initialized project assets and build targets for .NET 8.0.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");\
+
+ namespace HelloWorldApp
+ {
+    using System;
+    class Program
+    {
+        /// <summary>
+        /// The entry point of the application.
+        /// Writes a greeting message and a prompt for generating addition and subtraction equations to the console.
+        /// </summary>
+        /// <param name="args">An array of command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Generate equations for addition and subtraction:");
+
+        }
+    }
+ }
